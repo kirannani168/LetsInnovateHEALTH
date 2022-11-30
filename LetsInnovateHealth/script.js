af4cd4c86c8842470fabe6e0afa365dd6e48e141
Ravikiran Reddy Balemla <56499420+kirannani168@users.noreply.github.com>
2022-11-30 12:25:38 +0530

    Initial Commit

---
+++ LetsInnovateHealth/script.js
@@ -0,0 +1,12 @@
+let data = "<b>The alarm is set four times a day</b>"+
+                "<ul>"+
+                    "<li>Morning - BreakFast</li>"+
+                    "<li>Afternoon - Lunch</li>"+
+                    "<li>Evening - Snaks</li>"+
+                    "<li>Night - Dinner</li>"+
+                "</ul>"
+
+function alarmSet(){
+    document.getElementById('testt').innerHTML = data
+    alert("Alarm Set Successfully");
+}
